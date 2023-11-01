ff73f86a6010b9164c810419a14ffe6c73a29c68
computej <computejpdc@gmail.com>
2023-11-01 11:00:56 -0700

    replace certain integers with a certain string

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,20 @@ function beepBoop(number) {
   if(number === 0) {
     return [ 0 ];
   }
-    let outArray = new Array();
-    for(i = 0; i <= number; i++){
-      outArray.push(i);
+  let outArray = new Array();
+  debugger;
+  for(i = 0; i <= number; i++){
+    let j = i.toString();
+    if(j.includes("3")) {
+      j = "Won't you be my neighbor?";
     }
+    else if(j.includes("2")) {
+      j = "Boop!";
+    }
+    else if(j.includes("1")) {
+      j = "Beep!";
+    }
+    outArray.push(parseInt(j) === i ? i : j);
+  }
   return outArray;
 }
