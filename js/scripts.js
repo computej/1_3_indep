46db74aa79040e66c72a2b5af59b61ff25e0d953
computej <computejpdc@gmail.com>
2023-11-03 11:13:41 -0700

    add new function showOutput

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function beepBoop(number) {
     return [ 0 ];
   }
   let outArray = new Array();
-  debugger;
   for(i = 0; i <= number; i++){
     let j = i.toString();
     if(j.includes("3")) {
@@ -24,3 +23,17 @@ function beepBoop(number) {
 
 // UI
 
+function showOutput(number) {
+  let beepArray = beepBoop(number);
+  if (beepArray) {
+    debugger;
+    let outputBox = document.querySelector("#output-box");
+    let ul = document.createElement("ul")
+    beepArray.forEach(function(element, index) {
+      let listItem = document.createElement("li");
+      listItem.textContent = beepArray[index].toString();
+      ul.append(listItem);
+    });
+    outputBox.append(ul);
+  }
+}
