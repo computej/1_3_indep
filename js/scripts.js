9825583a3f921919064c050940584637855ec02a
computej <computejpdc@gmail.com>
2023-11-07 09:46:11 -0800

    make the output have use a bootstrap list group
    this also moves the list deleter to showOutput

--- js/scripts.js
+++ js/scripts.js
@@ -27,10 +27,14 @@ function showOutput(number) {
   let beepArray = beepBoop(number);
   if (beepArray) {
     //debugger;
+    let outputList = document.querySelector("#output-box ul");
+    if(outputList) { outputList.remove(); }
     let outputBox = document.querySelector("#output-box");
-    let ul = document.createElement("ul")
+    let ul = document.createElement("ul");
+    ul.setAttribute("class", "list-group");
     beepArray.forEach(function(element, index) {
       let listItem = document.createElement("li");
+      listItem.setAttribute("class", "list-group-item");
       listItem.textContent = beepArray[index].toString();
       ul.append(listItem);
     });
@@ -43,8 +47,6 @@ window.addEventListener("load", function(event) {
   startButton.addEventListener("click", function(btnevent) {
     debugger;
     btnevent.preventDefault();
-    let outputList = document.querySelector("#output-box ul");
-    if(outputList) { outputList.remove(); }
     const beepNumber = parseInt(document.getElementById("beep-amount").value);
     if(beepNumber > 0) { showOutput(beepNumber); }
   });
